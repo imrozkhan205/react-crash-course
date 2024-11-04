701306fa7d5791bea3ac6867855b902424139fae
imrozkhan205 <imrozkhan2258@gmail.com>
2024-11-04 23:59:00 +0530

    day-3

--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -2,6 +2,12 @@ import {TodoCard} from "./TodoCard"
 export function TodoList(props){
     const { todos } = props
     const tab = 'All'
+    const filterTodoList = tab === 'All' ?
+    todos:
+    tab === 'Completed' ?
+    todos.filter(val => val.completed):
+    todos.filter(val => !val.completed)
+
     return (
         <>
             {todos.map((todo, todoIndex)=>{
